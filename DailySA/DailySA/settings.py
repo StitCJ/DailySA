401eaad9797a7605f06bb7e4d0b5044538f83595
StitCJ <dlckdwns0417@gmail.com>
2022-12-24 14:22:45 +0900

    cicd 테스트

--- DailySA/DailySA/settings.py
+++ DailySA/DailySA/settings.py
@@ -81,13 +81,13 @@ WSGI_APPLICATION = "DailySA.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = my_settings.DATABASES
-#DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.sqlite3",
-#        "NAME": BASE_DIR / "db.sqlite3",
-#    }
-#}
+#DATABASES = my_settings.DATABASES
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Password validation
