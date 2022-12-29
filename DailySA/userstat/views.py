2ec40dd91bac6af20dc147984f7ec43a9946d803
StitCJ <dlckdwns0417@gmail.com>
2022-12-29 12:23:53 +0900

    mypage update

--- DailySA/userstat/views.py
+++ DailySA/userstat/views.py
@@ -6,10 +6,15 @@ from main.models import Problem
 def mypage(request):
     user = request.user
     user_solved = Solved.objects.filter(user_id=user)
+    not_solved = Solved.objects.exclude(user_id=user)
     n = 0
     pklst = []
     for i in user_solved :
         pklst.append(i.problem_id.pk)
         n += 1
-    print(pklst)
-    return render(request, 'userstat/mypage.html', {'nsolved': n, 'pklst': pklst})
+    nn = 0
+    npklst = []
+    for i in not_solved :
+        npklst.append(i.problem_id.pk)
+        nn += 1
+    return render(request, 'userstat/mypage.html', {'nsolved': n, 'pklst': pklst, 'nnsolved': nn, 'npklst': npklst})
